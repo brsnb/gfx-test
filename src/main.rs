97c49a7aa474594d6a70a1e5e74098579cd6758e
brsnb <49287229+brsnb@users.noreply.github.com>
2020-09-02 20:11:39 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+
 use gfx_hal::{device, window, Instance};
 
 const DIMS: window::Extent2D = window::Extent2D {
@@ -6,43 +9,72 @@ const DIMS: window::Extent2D = window::Extent2D {
 };
 
 fn main() {
-    let event_loop = winit::event_loop::EventLoop::new();
+    env_logger::init();
 
-    let window = winit::window::WindowBuilder::new()
-        .with_min_inner_size(winit::dpi::Size::Logical(winit::dpi::LogicalSize::new(
-            64.0, 64.0,
-        )))
-        .with_inner_size(winit::dpi::Size::Physical(winit::dpi::PhysicalSize::new(
-            DIMS.width,
-            DIMS.height,
-        )))
-        .with_title("triangle".to_string())
-        .build(&event_loop)
-        .expect("Could not create window");
+    info!("Starting");
 
-    let (instance, mut adapters, surface) = {
-        let instance =
-            backend::Instance::create("triangle", 1).expect("Could not create instance");
-        let adapters = instance.enumerate_adapters();
-        let surface = unsafe {
-            instance
-                .create_surface(&window)
-                .expect("Could not create surface")
-        };
+    let window = Window::new().unwrap();
 
-        (instance, adapters, surface)
-    };
-
-    let adapter = adapters.remove(0);
 }
 
-struct Context<B: gfx_hal::Backend> {
+pub struct Window {
+    event_loop: winit::event_loop::EventLoop<()>,
+    window: winit::window::Window,
+}
+
+impl Window {
+    pub fn new() -> Result<Self, &'static str> {
+        let event_loop = winit::event_loop::EventLoop::new();
+
+        let window = winit::window::WindowBuilder::new()
+            .with_min_inner_size(winit::dpi::Size::Logical(winit::dpi::LogicalSize::new(
+                64.0, 64.0,
+            )))
+            .with_inner_size(winit::dpi::Size::Physical(winit::dpi::PhysicalSize::new(
+                DIMS.width,
+                DIMS.height,
+            )))
+            .with_title("triangle".to_string())
+            .build(&event_loop)
+            .map_err(|_| "Could not create window")?;
+
+        Ok(
+            Window {
+                event_loop,
+                window,
+            }
+        )
+    }
+}
+
+pub struct Renderer<B: gfx_hal::Backend> {
     instance: B::Instance,
+    adapter: gfx_hal::adapter::Adapter<B>,
+    device: B::Device,
 }
 
-impl<B> Context<B>
+impl<B> Renderer<B>
 where
     B: gfx_hal::Backend
 {
+    pub fn new(window: &winit::window::Window) -> Self {
+
+        let (instance, mut adapters, surface) = {
+            let instance =
+                backend::Instance::create("triangle", 1).expect("Could not create instance");
+            let adapters = instance.enumerate_adapters();
+            let surface = unsafe {
+                instance
+                    .create_surface(window)
+                    .expect("Could not create surface")
+            };
+
+            (instance, adapters, surface)
+        };
+
+        let adapter = adapters.remove(0);
+        let memory_types = adapter.physical_device.memory_properties().memory_types;
+        let limits = adapter.physical_device.limits();
+    }
 
 }
