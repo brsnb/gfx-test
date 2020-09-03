3e63f5dca503516d5b1a3c637d9eb50cac952ae9
brsnb <49287229+brsnb@users.noreply.github.com>
2020-09-03 09:19:56 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,47 @@ const DIMS: window::Extent2D = window::Extent2D {
 fn main() {
     env_logger::init();
 
-    info!("Starting");
-
     let window = Window::new().unwrap();
 
+    info!("Starting event loop");
+
+    // TODO: Some sort of main loop struct instead of just windowing info??
+    window.event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Poll;
+
+        match event {
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::CloseRequested => {
+                    info!("Exiting...");
+                    *control_flow = winit::event_loop::ControlFlow::Exit
+                }
+                winit::event::WindowEvent::KeyboardInput {
+                    input:
+                        winit::event::KeyboardInput {
+                            virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => {
+                    info!("Exit requested via keypress");
+                    *control_flow = winit::event_loop::ControlFlow::Exit;
+                }
+                winit::event::WindowEvent::Resized(dims) => {
+                    // TODO: Recreate swapchain
+                }
+                _ => {}
+            },
+            winit::event::Event::RedrawRequested(_) => {
+                // TODO: Render
+            }
+            _ => {}
+        }
+    });
 }
 
 pub struct Window {
-    event_loop: winit::event_loop::EventLoop<()>,
-    window: winit::window::Window,
+    pub event_loop: winit::event_loop::EventLoop<()>,
+    pub window: winit::window::Window,
 }
 
 impl Window {
@@ -38,27 +70,25 @@ impl Window {
             .build(&event_loop)
             .map_err(|_| "Could not create window")?;
 
-        Ok(
-            Window {
-                event_loop,
-                window,
-            }
-        )
+        Ok(Window { event_loop, window })
     }
 }
+
+
 
 pub struct Renderer<B: gfx_hal::Backend> {
     instance: B::Instance,
     adapter: gfx_hal::adapter::Adapter<B>,
     device: B::Device,
+    surface: B::Surface,
 }
 
 impl<B> Renderer<B>
 where
-    B: gfx_hal::Backend
+    B: gfx_hal::Backend,
 {
+    // FIXME: winit vs raw window handle
     pub fn new(window: &winit::window::Window) -> Self {
-
         let (instance, mut adapters, surface) = {
             let instance =
                 backend::Instance::create("triangle", 1).expect("Could not create instance");
@@ -76,5 +106,4 @@ where
         let memory_types = adapter.physical_device.memory_properties().memory_types;
         let limits = adapter.physical_device.limits();
     }
-
 }
